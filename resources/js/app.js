efbc387c02ad1035f4b7d9ec9c1d434d6367c423
zohair22 <Zohairosama321@gmail.com>
2022-05-21 14:48:00 +0200

    existing file

---
+++ resources/js/app.js
@@ -0,0 +1,26 @@
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import {InertiaProgress} from "@inertiajs/progress";
+
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+});
+InertiaProgress.init({
+  // The delay after which the progress bar will
+  // appear during navigation, in milliseconds.
+  delay: 250,
+
+  // The color of the progress bar.
+  color: 'red',
+
+  // Whether to include the default NProgress styles.
+  includeCSS: true,
+
+  // Whether the NProgress spinner will be shown.
+  showSpinner: true,
+})
